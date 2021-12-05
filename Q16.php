7bc41dd2d8b8ef392ede3c0742fe2feb7ffe5df2
Andreas Loizides <andreas16700@icloud.com>
2021-12-05 22:39:47 +0200

    Add styling

--- Q16.php
+++ Q16.php
@@ -17,6 +17,8 @@
 
 <html>
 <head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="main.css">
 	<style>
 		table th{background:grey}
 		table tr:nth-child(odd){background:LightYellow}
@@ -120,7 +122,8 @@
 	<form method="post"> 
 		<input type="submit" name="disconnect" value="Disconnect"/> 
 		<input type="submit" value="Menu" formaction="connect.php">
-	</form> 
-
+	</form>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
 </body>
 </html>
--- Q1SUPER.php
+++ Q1SUPER.php
@@ -2,6 +2,10 @@
 	session_start(); 
 ?>
 <html>
+<head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="main.css">
+</head>
 <body>
 <?php
 	
@@ -43,7 +47,8 @@
 	
 	<form method="post"> 
 		<input type="submit" name="disconnect" value="LOGOUT"/> 
-	</form> 
-
+	</form>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
 </body>
 </html>
